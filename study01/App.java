16b8328066cd16ad83dffe5f1d609bd411cdcba8
HelenHam <hh0802@naver.com>
2024-12-31 12:00:36 +0900

    class, 객체, if문, for문, 형식을 선언하지 않은 변수의 자동 선언

--- study01/App.java
+++ study01/App.java
@@ -6,10 +6,34 @@ public class App {
         System.out.println("안녕하세요.");
         int 정수 = 1;
         final int 상수 = 2;
+
+        모듈 객체 = new 모듈();
+        System.out.println(객체.기능1(1,4));
+
+        int a = 1;
+        String b = "1";
+
+        // if (a == b){}
+
+        for (int i = 0; i<5 ; i++){
+            System.out.println(i);
+        }
     }
     
     int add(int a, int b){
         return a + b;
     }
 
+}
+
+class 모듈 {
+    public int 기능1(int a, int b) {
+        return a + b;
+    }
+    public int 기능2(int a, int b) {
+        return a + b;
+    }
+    public int 기능3(int a, int b) {
+        return a + b;
+    }
 }
--- study01/App.js
+++ study01/App.js
@@ -19,3 +19,44 @@ const add3 = add;
 
 console.log(add2(1,3));
 console.log(add3(1,3));
+
+var 모듈 = {
+    기능1 : function(a, b) {return a + b},
+    기능2 : (a, b) => {return a + b},
+    기능3: (a, b) => a + b
+}
+
+console.log(모듈.기능1(1,4));
+console.log(모듈.기능2(1,4));
+console.log(모듈.기능3(1,4));
+
+class 모듈2 {
+    constructor() {}
+    기능1 (a, b) {
+        return a + b
+    };
+    기능2 (a, b) {
+        return a + b
+    };
+    기능3 (a, b) {
+        return a + b
+    };
+}
+
+const 객체 = new 모듈2();
+console.log(객체.기능1(1,4));
+
+a = 1;
+b = "1";
+
+if(a === b) {
+    console.log("참")
+} else {
+    console.log("거짓")
+}
+
+for (i = 0 ; i < 5 ; i++){
+    console.log(i);
+}
+
+console.log(i);
